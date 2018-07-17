b32d3ddd3b1d7f5885a7dcc9b770523b5dc555dc
Bogdan Gerashchenko <b.gerashchenko@gmail.com>
2018-07-17 21:19:26 +0300

    Add image upload action

--- src/main/java/org/ontario/spring5recipeapp/controllers/ImageController.java
+++ src/main/java/org/ontario/spring5recipeapp/controllers/ImageController.java
@@ -2,7 +2,10 @@ package org.ontario.spring5recipeapp.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.ontario.spring5recipeapp.services.ImageService;
+import org.ontario.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,18 +16,27 @@ import org.springframework.web.multipart.MultipartFile;
 public class ImageController {
 
     private ImageService imageService;
+    private RecipeService recipeService;
 
-    public ImageController(ImageService imageService) {
+    public ImageController(ImageService imageService, RecipeService recipeService) {
         this.imageService = imageService;
+        this.recipeService = recipeService;
     }
 
-    @PostMapping("/recipe/{id}/iamge")
+    @GetMapping("recipe/{id}/image")
+    public String showUploadFormAction(Model model, @PathVariable String id) {
+        log.debug("ImageController uploadFormAction, recipeId - " + id);
+        model.addAttribute("recipe", recipeService.findCommandById(Long.parseLong(id)));
+        return "recipe/imageUploadForm";
+    }
+
+    @PostMapping("/recipe/{id}/image")
     public String handleImageString(@PathVariable String id,
-                                    @RequestParam MultipartFile file) {
+                                    @RequestParam("imagefile") MultipartFile file) {
 
         log.debug("ImageController -> upload image");
         imageService.saveImageFile(Long.parseLong(id), file);
 
-        return "recipe/" + id + "/view";
+        return "redirect:/recipe/" + id + "/view";
     }
 }
--- src/test/java/org/ontario/spring5recipeapp/controllers/ImageControllerTest.java
+++ src/test/java/org/ontario/spring5recipeapp/controllers/ImageControllerTest.java
@@ -4,19 +4,30 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.ontario.spring5recipeapp.commands.RecipeCommand;
 import org.ontario.spring5recipeapp.services.ImageService;
+import org.ontario.spring5recipeapp.services.RecipeService;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class ImageControllerTest {
 
     @Mock
     private ImageService imageService;
+    @Mock
+    private RecipeService recipeService;
     private ImageController controller;
 
     private MockMvc mockMvc;
@@ -25,17 +36,35 @@ public class ImageControllerTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
-        this.controller = new ImageController(imageService);
+        this.controller = new ImageController(imageService, recipeService);
         this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void testGetImageForm() throws Exception {
+        // given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        // when
+        mockMvc.perform(get("/recipe/1/image"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"));
+
+        verify(recipeService, times(1)).findCommandById(anyLong());
     }
 
     @Test
     public void testHandleImagePost() throws Exception {
         MockMultipartFile multipartFile =
-                new MockMultipartFile("file", "testing.txt", "text/plain", "Hello World from SpringBoot".getBytes());
+                new MockMultipartFile("imagefile", "testing.txt", "text/plain", "Hello World from SpringBoot".getBytes());
 
         mockMvc.perform(multipart("/recipe/1/image").file(multipartFile))
-               .andExpect(status().isFound())
-               .andExpect(header().string("location", "/"));
+               .andExpect(status().is3xxRedirection())
+               .andExpect(header().string("Location", "/recipe/1/view"));
+
+        verify(imageService, times(1)).saveImageFile(anyLong(), any());
     }
 }
